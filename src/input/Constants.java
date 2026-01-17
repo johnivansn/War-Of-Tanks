ae472d44bbbde3d5575ed2c8c08662d3e5578d36
johnivansn <johnivansn@gmail.com>
2026-01-17 13:20:30 -0400

    fix: Use cross-platform paths in Constants
    - Replaces backslash concatenation with Paths.get()
    - Compatible with Windows, Linux, and macOS
    - Removes dependency on FileSystemView
    - Automatically uses system path separators
    - Centralizes logic in getAppDirectory()

--- src/input/Constants.java
+++ src/input/Constants.java
@@ -1,6 +1,7 @@
 package input;
 
-import javax.swing.filechooser.FileSystemView;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Constants {
 
@@ -9,9 +10,9 @@ public class Constants {
 	public static final int WIDTH = 1100;
 	public static final int HEIGHT = 650;
 	public static int SELECT = 1;
-	
+
 	// player properties
-	
+
 	public static final int FIRE_RATE = 300;
 	public static final double DELTA_ANGLE = 0.1;
 	public static final double ACC = 0.2;
@@ -56,14 +57,28 @@ public class Constants {
 	public static final String SCORE = "SCORE";
 	public static final String DATE = "DATE";
 	public static final String NICKNAME = "NICKNAME";
-	// data.xml if you use XMLParser
-	public static final String SCORE_PATH = FileSystemView.getFileSystemView().getDefaultDirectory().getPath()
-			+ "\\War_Of_Tanks\\dates\\data.json";
-	public static final String SAVE_CARP = FileSystemView.getFileSystemView().getDefaultDirectory().getPath()
-			+ "\\War_Of_Tanks\\edits";
-	public static final String BIN = FileSystemView.getFileSystemView().getDefaultDirectory().getPath()
-			+ "\\War_Of_Tanks\\BIN";
-	//
+
+	// Paths multiplataforma
+	private static final String USER_HOME = System.getProperty("user.home");
+	private static final String APP_DIR = "War_Of_Tanks";
+
+	private static Path getAppDirectory() {
+		return Paths.get(USER_HOME, "Documents", APP_DIR);
+	}
+
+	public static final String SCORE_PATH = getAppDirectory()
+			.resolve("dates")
+			.resolve("data.json")
+			.toString();
+
+	public static final String SAVE_CARP = getAppDirectory()
+			.resolve("edits")
+			.toString();
+
+	public static final String BIN = getAppDirectory()
+			.resolve("BIN")
+			.toString();
+
 	public static final long POWER_UP_DURATION = 10000;
 	public static final long POWER_UP_SPAWN_TIME = 8000;
 
