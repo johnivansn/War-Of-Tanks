1db40bcc41b606ce0998f1222687caf5dfc37eef
johnivansn <johnivansn@gmail.com>
2026-01-17 12:59:41 -0400

    perf: Caching resized images in the Button constructor
    - Pre-calculate scaled image in the constructor (once)
    - Eliminates calls to Resize.getResize() in draw() (60 times/sec)
    - Adds a configurable HOVER_SCALE constant
    - Adds a dispose() method for memory management
    - Documents cache optimization

--- src/ui/Button.java
+++ src/ui/Button.java
@@ -13,40 +13,42 @@ import graphics.Resize;
 import graphics.Text;
 import input.MouseInput;
 
-public class Button{
+public class Button {
 
 	private BufferedImage mouseOutImg;
 	private BufferedImage mouseInImg;
+	private BufferedImage mouseInImgScaled;
 	public boolean mouseIn, resize;
 	private Rectangle boundingBox;
 	private Action action;
 	private String text;
 	private int x, y;
+	private static final double HOVER_SCALE = 1.05;
 
 	public Button(
-			BufferedImage mouseOutImg, 
-			BufferedImage mouseInImg, 
-			int x, 
-			int y, 
-			String text, 
-			Action action)
-	{
+			BufferedImage mouseOutImg,
+			BufferedImage mouseInImg,
+			int x,
+			int y,
+			String text,
+			Action action) {
 		this.mouseInImg = mouseInImg;
 		this.mouseOutImg = mouseOutImg;
 		this.text = text;
 		this.x = x;
 		this.y = y;
-		
+
 		boundingBox = new Rectangle(
 				x,
-				y, 
-				mouseInImg.getWidth(), 
+				y,
+				mouseInImg.getWidth(),
 				mouseInImg.getHeight());
-		
-		this.action = action;
-	}
-	public void update() {
 
+		this.action = action;
+		this.mouseInImgScaled = Resize.getResize(mouseInImg, HOVER_SCALE);
+	}
+
+	public void update() {
 		if (boundingBox.contains(MouseInput.X, MouseInput.Y)) {
 			mouseIn = true;
 		} else {
@@ -60,53 +62,59 @@ public class Button{
 
 	public void draw(Graphics g) {
 		if (mouseIn) {
-			
-			BufferedImage textureR = Resize.getResize(mouseInImg, 1.05);
-			
+			int offsetX = (mouseInImgScaled.getWidth() - boundingBox.width) / 2;
+			int offsetY = (mouseInImgScaled.getHeight() - boundingBox.height) / 2;
+
 			g.drawImage(
-					textureR, 
-					(int) (boundingBox.x - (textureR.getWidth() - boundingBox.getWidth()) / 2),
-					(int) (boundingBox.y - (textureR.getHeight() - boundingBox.getHeight()) / 2),
+					mouseInImgScaled,
+					boundingBox.x - offsetX,
+					boundingBox.y - offsetY,
 					null);
-			//	Necesario??
-			
+
+			// Texto con fuente ligeramente más grande para hover
 			Text.drawText(
-					g, 
-					text, 
+					g,
+					text,
 					new Vector2D(
 							boundingBox.getX() + boundingBox.getWidth() / 2,
-							boundingBox.getY() + boundingBox.getHeight() / 1.2), 
-					true, 
-					Color.BLACK, 
+							boundingBox.getY() + boundingBox.getHeight() / 1.2),
+					true,
+					Color.BLACK,
 					Assets.fontMed05);
-			
-			if(resize) {
-			Graphics2D g2d = (Graphics2D) g;
-			g2d.setStroke(new BasicStroke(5));
-			g.setColor(Color.BLACK);
-			g.drawRect(
-				x, 
-				y, 
-				textureR.getWidth(), 
-				textureR.getHeight());
+
+			// Borde opcional si resize está activo
+			if (resize) {
+				Graphics2D g2d = (Graphics2D) g;
+				g2d.setStroke(new BasicStroke(5));
+				g.setColor(Color.BLACK);
+				g.drawRect(
+						x,
+						y,
+						mouseInImgScaled.getWidth(),
+						mouseInImgScaled.getHeight());
 			}
-			
+
 		} else {
+			// Estado normal
 			g.drawImage(
-					mouseOutImg, 
-					boundingBox.x, 
-					boundingBox.y, 
+					mouseOutImg,
+					boundingBox.x,
+					boundingBox.y,
 					null);
-			
+
 			Text.drawText(
-					g, 
-					text, 
+					g,
+					text,
 					new Vector2D(
 							boundingBox.getX() + boundingBox.getWidth() / 2,
-							boundingBox.getY() + boundingBox.getHeight() / 1.2), 
-					true, 
-					Color.BLACK, 
+							boundingBox.getY() + boundingBox.getHeight() / 1.2),
+					true,
+					Color.BLACK,
 					Assets.fontMed);
 		}
+	}
+
+	public void dispose() {
+		mouseInImgScaled = null;
 	}
 }
